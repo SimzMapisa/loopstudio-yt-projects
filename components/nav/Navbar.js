61cfac2b3d36435ae78544a2eddf03d34b8a991d
SimzMapisa <simbarashe795@gmail.com>
2023-01-05 02:20:56 +0000

    add navabar component for mobile devices

--- components/nav/Navbar.js
+++ components/nav/Navbar.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import styles from './Navbar.module.css';
+import Image from 'next/image';
+import logo from '../../public/images/logo.svg';
+import menuIcon from '../../public/images/icon-hamburger.svg';
 
 const Navbar = () => {
 	return (
 		<nav className={styles.navbar}>
-			<div className={styles.container}>Navbar</div>
+			<div className={styles.container}>
+				<div className={styles.logo}>
+					<Image src={logo} alt='next logo' />
+				</div>
+				<div className={styles.menu}>
+					<Image src={menuIcon} alt='menu icon' />
+				</div>
+			</div>
 		</nav>
 	);
 };
